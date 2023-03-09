84dc4efc40017d0cd0105d92604362079552f3e1
SourabhSantoshKamble <sourabhsantoshkamble@gmail.com>
2023-03-09 18:49:45 +0530

    Hello World code snippet

---
+++ Hello World/HelloWorld.java
@@ -0,0 +1,14 @@
+// Program to print Hello World
+
+// HelloWorld class
+class HelloWorld{
+
+    // main function
+    public static void main (String args[]){
+
+        // Printing statement
+        System.out.println("\nHello World!\n");
+
+    }   // main function
+
+}   // HelloWorld Class
